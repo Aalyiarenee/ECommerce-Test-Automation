10473b7b2f30cadcdd736c9282be2fdacd35b870
Aalyia Castle <aalyiarenee@gmail.com>
2026-07-27 19:35:44 -0400

    Add remove-from-cart test scenario

--- EcommerceTests/UnitTest1.cs
+++ EcommerceTests/UnitTest1.cs
@@ -50,4 +50,21 @@ public class LoginTests : PageTest
         await Expect(Page.Locator(".inventory_item_name"))
             .ToHaveTextAsync("Sauce Labs Backpack");
     }
+    [Test]
+public async Task ValidUser_ShouldRemoveProductFromCart()
+{
+    await Page.GotoAsync("https://www.saucedemo.com/");
+
+    await Page.Locator("#user-name").FillAsync("standard_user");
+    await Page.Locator("#password").FillAsync("secret_sauce");
+    await Page.Locator("#login-button").ClickAsync();
+
+    await Page.Locator("#add-to-cart-sauce-labs-backpack").ClickAsync();
+    await Page.Locator(".shopping_cart_link").ClickAsync();
+
+    await Page.Locator("#remove-sauce-labs-backpack").ClickAsync();
+
+    await Expect(Page.Locator(".inventory_item_name"))
+        .ToHaveCountAsync(0);
+    }
 }
